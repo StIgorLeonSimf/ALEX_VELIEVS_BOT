0f8c702c6eec632f8aa349da799212674d6fb596
StIgorLeonSimf <aligol6573@gmail.com>
2025-12-29 19:34:30 +0300

    second

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
-TOKEN = '8018328189:AAGKEReZMwnI1F0qU7IqZ7W5zNgnxQcxQ00'
-PAYMENTS_TOKEN = "1744374395:TEST:8f8132eccd7c8722f5d9"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('BOT_TOKEN')
+PAYMENTS_TOKEN = os.getenv('BOT_PAYMENTS_TOKEN')
+
+if not TOKEN:
+    raise ValueError("BOT_TOKEN не найден в .env файле!")
+
+if not PAYMENTS_TOKEN:
+    raise ValueError("BOT_PAYMENTS_TOKEN не найден в .env файле!")
+
+# TOKEN = '8018328189:AAGKEReZMwnI1F0qU7IqZ7W5zNgnxQcxQ00'
+# PAYMENTS_TOKEN = "1744374395:TEST:8f8132eccd7c8722f5d9"
