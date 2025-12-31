977503db54c35454ad3407fbbe7f2b1c01b10b46
StIgorLeonSimf <aligol6573@gmail.com>
2025-12-31 09:16:23 +0300

    config

--- config.py
+++ config.py
@@ -12,5 +12,4 @@ if not TOKEN:
 if not PAYMENTS_TOKEN:
     raise ValueError("BOT_PAYMENTS_TOKEN не найден в .env файле!")
 
-# TOKEN = '8018328189:AAGKEReZMwnI1F0qU7IqZ7W5zNgnxQcxQ00'
-# PAYMENTS_TOKEN = "1744374395:TEST:8f8132eccd7c8722f5d9"
+
